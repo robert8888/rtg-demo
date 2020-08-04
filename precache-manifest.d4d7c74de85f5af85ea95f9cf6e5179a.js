afba4f94dbb39b5256126f69751cb4f2a13cbffa
unknown <robert.kami88@gmail.com>
2020-08-04 17:14:53 +0200

    Updates

---
+++ precache-manifest.d4d7c74de85f5af85ea95f9cf6e5179a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba062ab0fec3d9ba8dc9b91e6bd5049a",
+    "url": "/rtg-demo/index.html"
+  },
+  {
+    "revision": "ed1bc46f37c707296859",
+    "url": "/rtg-demo/static/css/2.30c04a21.chunk.css"
+  },
+  {
+    "revision": "3f4c471d1e7b42527113",
+    "url": "/rtg-demo/static/css/main.fa83a8bb.chunk.css"
+  },
+  {
+    "revision": "ed1bc46f37c707296859",
+    "url": "/rtg-demo/static/js/2.73aad15f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rtg-demo/static/js/2.73aad15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4c471d1e7b42527113",
+    "url": "/rtg-demo/static/js/main.294ca091.chunk.js"
+  },
+  {
+    "revision": "04ab5fed1e4702c689e0",
+    "url": "/rtg-demo/static/js/runtime-main.28e2908b.js"
+  }
+]);
